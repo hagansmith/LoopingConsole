92d00f4100e79cabb7944f13b7b828c2f61b9b71
Adam Smith <hagansmith@gmail.com>
2018-01-15 20:15:01 -0600

    before refactor

---
+++ LoopingConsole/Child.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole
+{
+    class Child
+    {
+        public string Name { get; set; }
+        public bool Sick { get; set; }
+
+        public string ForgeASickNote()
+        {
+            return Sick
+               ? $"Plase excuse {Name} from class today because of illness"
+               : $"{Name}, you still have to go to school";
+        }
+    }
+}
--- LoopingConsole/Program.cs
+++ LoopingConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoopingConsole
 {
@@ -10,6 +6,17 @@ namespace LoopingConsole
     {
         static void Main(string[] args)
         {
+            // create a child with name of sally that is sick
+            var sally = new Child { Name = "Sally", Sick = true };
+            Console.WriteLine(sally.ForgeASickNote());
+            Console.WriteLine ($"{sally.Name} is {(sally.Sick ? "sick" : "not sick")}");
+
+            // create a second child with name of sally that is sick
+            var ralph = new Child { Name = "Ralph", Sick = false };
+            Console.WriteLine(ralph.ForgeASickNote());
+            Console.WriteLine($"{ralph.Name} is {(ralph.Sick ? "sick" : "not sick")}");
+
+            Console.ReadLine();
         }
     }
 }
